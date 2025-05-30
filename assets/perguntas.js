a85e5b29490b3a650140bb8e6a89ef6710989ac3
Izzy <isabely.bravo@escola.pr.gov.br>
2025-05-30 23:34:54 +0000

    Aula 06- Criando cartões com JS

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Tornados registrados',
+    'O que foi o Tornado Tri-State de 1925?',
+    'O Tornado Tri-State de 1925 foi um dos tornados mais mortais e destrutivos da história dos Estados Unidos, ocorrendo em 18 de março. A tempestade causou 695 mortes e feriu mais de 2.000 pessoas enquanto atravessava o Missouri, Illinois e Indiana.'
+)
+
+criaCartao(
+    'Tornados registrados',
+    'O que foi o tornado Bridge Creek-Moore de 1999?',
+    'O tornado Bridge Creek-Moore de 1999 foi um grande, duradouro e excepcionalmente poderoso. Considerado o tornado mais forte já registrado a afetar a área metropolitana, o tornado devastou porções do sul de Oklahoma City, bem como municípios vizinhos ao sul e sudoeste da cidade.'
+)
+
+criaCartao(
+    'Tornados registrados',
+    'O que foi o tornado El reno 2013?',
+    'O El Reno, Tornado de 2013, foi um evento meteorológico extremo que ocorreu a 31 de maio de 2013, perto de El Reno, Oklahoma. Foi o tornado mais largo registrado em todos os tempos, com uma largura de 2,6 milhas (4,2 km). O tornado também foi um dos mais poderosos já amostrados por radares meteorológicos móveis.'
+)
